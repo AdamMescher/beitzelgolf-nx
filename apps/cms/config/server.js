03e93851134d666b5d497ac335e333bfe09a35a0
Adam Mescher <admescher@gmail.com>
2022-06-17 18:11:39 -0400

    Update database config

--- apps/cms/config/server.js
+++ apps/cms/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  proxy: true,
+  url: env('MY_HEROKU_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
