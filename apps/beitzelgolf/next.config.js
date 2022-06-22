1e39feecbf5035e61d48e2edd0a4b6e962e9db27
Adam Mescher <admescher@gmail.com>
2022-06-22 13:29:23 -0600

    Remove strapi CMS

--- apps/beitzelgolf/next.config.js
+++ apps/beitzelgolf/next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+    images: {
+      domains: ['res.cloudinary.com'],
+    },
   },
 };
 
--- apps/cms/config/env/production/server.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-  proxy: true,
-  port: env('PORT'),
-  url: env('HEROKU_URL'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
