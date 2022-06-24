a6692c599e1d2dc5129b536772532f4ca69af316
Adam Mescher <admescher@gmail.com>
2022-06-24 14:07:48 -0600

    Keystone working with postgres locally

--- apps/keystone/keystone.ts
+++ apps/keystone/keystone.ts
@@ -1,30 +1,20 @@
-/*
-Welcome to Keystone! This file is what keystone uses to start the app.
-
-It looks at the default export, and expects a Keystone config object.
-
-You can find all the config options in our docs here: https://keystonejs.com/docs/apis/config
-*/
-
 import { config } from '@keystone-6/core';
-
-// Look in the schema file for how we define our lists, and how users interact with them through graphql or the Admin UI
 import { lists } from './schema';
-
-// Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
 import { withAuth, session } from './auth';
+import 'dotenv/config';
 
 export default withAuth(
-  // Using the config function helps typescript guide you to the available options.
   config({
-    // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: 'postgresql',
+      url:
+        process.env.DATABASE_URL ||
+        `postgres://${process.env.PG_USER}@localhost/beitzelgolf_keystone`,
+      enableLogging: true,
+      useMigrations: true,
+      idField: { kind: 'uuid' },
     },
-    // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
-      // For our starter, we check that someone has session data before letting them see the Admin UI.
       isAccessAllowed: (context) => !!context.session?.data,
     },
     lists,
