da8bd98da8e9dcd28bf95d8cbfd2b4e38b861f10
Adam Mescher <admescher@gmail.com>
2022-06-29 12:17:11 -0600

    Add previewHead property to add Google Fonts to UI storybook

--- libs/ui/.storybook/main.js
+++ libs/ui/.storybook/main.js
@@ -2,7 +2,12 @@ const rootMain = require('../../../.storybook/main');
 
 module.exports = {
   ...rootMain,
-
+  previewHead: (head) => `
+    ${head}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&display=swap" rel="stylesheet">
+  `,
   core: { ...rootMain.core, builder: 'webpack5' },
 
   stories: [
@@ -10,7 +15,11 @@ module.exports = {
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [...rootMain.addons, '@nrwl/react/plugins/storybook', '@storybook/addon-a11y'],
+  addons: [
+    ...rootMain.addons,
+    '@nrwl/react/plugins/storybook',
+    '@storybook/addon-a11y',
+  ],
   webpackFinal: async (config, { configType }) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {
